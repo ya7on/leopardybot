1a9ac7c98572203548cbd14820820df555c9dbbf
BehindLoader <hello@syoma.website>
2022-11-20 21:01:46 +0300

    Fix quiz marking on /restart

--- src/router/restart.rs
+++ src/router/restart.rs
@@ -3,6 +3,7 @@ use crate::game::base::GameHandler;
 use crate::router::base::RouteHandler;
 use crate::telebot::client::Client;
 use crate::telebot::typings::input::Update;
+use crate::texts::TextFormatter;
 use sea_orm::DatabaseConnection;
 
 #[derive(Clone)]
@@ -20,16 +21,36 @@ impl RouteHandler for RestartCommand {
             .message
             .as_ref()
             .ok_or_else(|| Error(format!("Invalid request. Missing message. {:?}", update)))?;
+        let user = message
+            .from
+            .as_ref()
+            .ok_or_else(|| Error(format!("Invalid request. Missing from. {:?}", update)))?;
 
         if GameHandler::exists(db, message.chat.id).await? {
             let game = GameHandler::get_by_chat_id(db, message.chat.id).await?;
             for poll in game.get_active_polls(db).await? {
-                client
+                if let Err(err) = client
                     .delete_message(message.chat.id, poll.message_id as usize)
-                    .await?;
+                    .await
+                {
+                    error!("Cannot delete message. {:?}", err);
+                }
                 GameHandler::mark_poll_as_handled(db, poll.id.clone()).await?;
             }
-            let question = GameHandler::get_question(db).await?;
+            let question =
+                if let Some(question) = GameHandler::get_new_question(db, user.id).await? {
+                    GameHandler::mark_quiz_as_played(db, user.id, question.id as isize).await?;
+                    question
+                } else {
+                    client
+                        .send_message(
+                            game.model.chat_id as isize,
+                            &TextFormatter::cannot_find_new_quiz()?,
+                        )
+                        .await?;
+                    GameHandler::get_question(db).await?
+                };
+
             let response = client
                 .send_quiz(
                     message.chat.id,
